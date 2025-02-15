daac66b7dfd3f9a3595b152e161643650201e7bd
Sebastian Stockton <96149632+sebiprograms@users.noreply.github.com>
2025-02-15 02:12:30 -0600

    Create report function
    This function is giving me some trouble, I've sorted the dict properly
    highest to lowest but to list its key and value with a string of text
    making a pretty report is giving me some issues. It has to do with
    accesing the key values pairs differently once its been conveerted
    to a list for sorting. Will keep trying.

--- main.py
+++ main.py
@@ -11,6 +11,17 @@ def charcount(text):
       chars[key] = 0
     chars[key] += 1
   print(chars)
+
+def report(text):
+  chars = {}
+  for key in text.lower():
+    if (key.isalpha()) & (key not in chars) & (key != ' '):
+      chars[key] = 0
+    if (key.isalpha()) & (key != ' '):
+      chars[key] += 1
+  sorted_report = sorted(chars.items())
+  for i in sorted_report:
+    print("The '" + sorted_report[i][0] + "' character was found " + sorted_report[i][1] + " times")
     
 
 def main():
@@ -19,6 +30,7 @@ def main():
     print(file_contents)
     wordcount(file_contents)
     charcount(file_contents)
+    report(file_contents)
 
 __name__ == '__main__' 
 
